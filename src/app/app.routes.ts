e07f86d256efec4e13f37deb5f908c09763b274a
FIXED-TERM Xu Minye (PS-BA/PJ-XRC) <xum5fe@bosch.com>
2023-12-19 15:38:21 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CompanyInfoComponent } from './company-info/company-info.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'boschtw', component: LoginComponent },
+    { path: 'company-info', component: CompanyInfoComponent},
+];
